b92e7783391a1af9cd78518437f076d7c0de5cbf
Josta Yee <github@josta.me>
2019-07-20 16:14:07 +0800

    Update genkey.sh

--- genkey.sh
+++ genkey.sh
@@ -2,5 +2,5 @@
 
 openssl req -newkey rsa:2048 -nodes -x509 -days 365 \
 	-keyout key.pem -out cert.pem \
-	-subj "/C=CN/ST=ZJ/L=HZ/O=NetEase/OU=DEV/CN=163.com/emailAddress=hzmingcheng@crop.netease.com"
+	-subj "/C=SG/ST=SG/L=SG/O=SGP/OU=DEV/CN=josta.me/emailAddress=syncthing@josta.me"
 
